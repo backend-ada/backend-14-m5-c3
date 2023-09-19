ded65c1ea574f130bf34cffed7402a3ad7075dad
Matias Arno <amatiasarno@gmail.com>
2023-09-19 19:36:32 -0300

    third commit

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -21,6 +21,54 @@ abstract class MovieController {
 
 		res.status(200).json(movieFound);
 	}
+
+	static create(req: any, res: any) {
+		const { name, year, director, cast, rating } = req.body;
+		const id = randomUUID();
+
+		db.movies.push({ id, name, year, director, cast, rating });
+
+		writeFile('./src/database/movies.json', db)
+			.then((data) => {
+				console.log(data);
+				return res.status(201).json({ id, name });
+			})
+			.catch((err) => {
+				return res.status(500).json({ message: 'Something went wrong', err });
+			});
+	}
+
+	static update(req: any, res: any) {
+		const { id } = req.params;
+		const { name, year, director, cast, rating } = req.body;
+
+		const movieToUpdate = db.movies.find((movie: any) => movie.id == id);
+
+		if (!movieToUpdate)
+			return res.status(404).json({ error: 'Movie not found' });
+
+		if (name) movieToUpdate.name = name;
+		if (year) movieToUpdate.year = year;
+		if (director) movieToUpdate.director = director;
+		if (cast) movieToUpdate.cast = cast;
+		if (rating) movieToUpdate.rating = rating;
+
+		writeFile('./src/database/movies.json', db)
+			.then((data) => {
+				console.log(data);
+				return res.status(200).json({
+					message: 'Updated successfully',
+					movie: movieToUpdate,
+				});
+			})
+			.catch((err) => {
+				return res.status(500).json({ message: 'Something went wrong', err });
+			});
+	}
+
+	static error(req: any, res: any) {
+		res.status(404).json({ message: 'Requested URL not found' });
+	}
 }
 
 export default MovieController;
--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -4,54 +4,11 @@ import MovieController from '../controllers/movies';
 export const movieRouter = Router();
 
 movieRouter.get('/', MovieController.getInfo);
-
 movieRouter.get('/movies', MovieController.getAll);
-
 movieRouter.get('/movies/:id', MovieController.getById);
 
-// movieRouter.post('/api/movies', (req, res) => {
-// 	const { name, year, director, cast, rating } = req.body;
-// 	const id = randomUUID();
+movieRouter.post('/api/movies', MovieController.create);
 
-// 	db.movies.push({ id, name, year, director, cast, rating });
+movieRouter.patch('/api/movies/:id', MovieController.update);
 
-// 	writeFile('./src/database/movies.json', db)
-// 		.then((data) => {
-// 			console.log(data);
-// 			return res.status(201).json({ id, name });
-// 		})
-// 		.catch((err) => {
-// 			return res.status(500).json({ message: 'Something went wrong', err });
-// 		});
-// });
-
-// movieRouter.patch('/api/movies/:id', (req, res) => {
-// 	const { id } = req.params;
-// 	const { name, year, director, cast, rating } = req.body;
-
-// 	const movieToUpdate = db.movies.find((movie: any) => movie.id == id);
-
-// 	if (!movieToUpdate) return res.status(404).json({ error: 'Movie not found' });
-
-// 	if (name) movieToUpdate.name = name;
-// 	if (year) movieToUpdate.year = year;
-// 	if (director) movieToUpdate.director = director;
-// 	if (cast) movieToUpdate.cast = cast;
-// 	if (rating) movieToUpdate.rating = rating;
-
-// 	writeFile('./src/database/movies.json', db)
-// 		.then((data) => {
-// 			console.log(data);
-// 			return res.status(200).json({
-// 				message: 'Updated successfully',
-// 				movie: movieToUpdate,
-// 			});
-// 		})
-// 		.catch((err) => {
-// 			return res.status(500).json({ message: 'Something went wrong', err });
-// 		});
-// });
-
-// movieRouter.use('*', (req, res) => {
-// 	res.status(404).json({ message: 'Requested URL not found' });
-// });
+movieRouter.use('*', MovieController.error);
